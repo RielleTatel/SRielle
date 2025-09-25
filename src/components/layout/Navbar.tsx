ff343b83b134eb61309e84c2a7ceea40e1f787bb
RielleTatel <tatelgabrielle19@gmail.com>
2025-09-26 01:32:46 +0800

    Back-up

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+const Navbar = () => {
+
+    return (
+        <>
+            <div className="  flex items-center justify-center content-center bg-amber-400">
+                <h1> hello </h1>
+            </div>
+        </>
+    )
+}
+
+export default Navbar; 
